34f09b52488653865e13b04a8b50f4cea021958c
Joon Sunn Foo <joonsunn.foo@gmail.com>
2025-12-22 18:24:06 +0800

    chore: add more banks to banks list

--- constants/bank-list.ts
+++ constants/bank-list.ts
@@ -1,4 +1,4 @@
-export const BankList = [
+export const BankList: BankListOption[] = [
   {
     label: "Ryt Bank",
     value: "ryt-bank",
@@ -17,6 +17,66 @@ export const BankList = [
     color: "white",
     backgroundColor: "#770F1C",
   },
+  { label: "Public Bank", value: "public-bank", backgroundColor: "white", color: "#e72626" },
+  {
+    label: "RHB Bank",
+    value: "rhb-bank",
+    color: "white",
+    backgroundColor: "#0062AE",
+  },
+  {
+    label: "Hong Leong Bank",
+    value: "hlb",
+    color: "white",
+    backgroundColor: "#15338E",
+  },
+  {
+    label: "AEON Bank",
+    value: "aeon-bank",
+    color: "white",
+    backgroundColor: "#EF25BC",
+  },
+  {
+    label: "GXBank",
+    value: "gxbank",
+    color: "#B90EBF",
+    backgroundColor: "#0E0122",
+  },
+  {
+    label: "HSBC Bank",
+    value: "hsbc-bank",
+    backgroundColor: "white",
+    color: "#e72626",
+  },
+  {
+    label: "Deutsche Bank",
+    value: "deutsche-bank",
+    color: "#33339E",
+    backgroundColor: "white",
+  },
+  {
+    label: "Setel",
+    value: "setel",
+    color: "white",
+    backgroundColor: "#00B0FE",
+  },
+  {
+    label: "ShopeePay",
+    value: "shopeepay",
+    color: "#FF3714",
+    backgroundColor: "white",
+  },
+  {
+    label: "Touch 'n Go eWallet",
+    value: "touch-n-go-ewallet",
+    color: "white",
+    backgroundColor: "#0D60A8",
+  },
 ];
 
-export type BankListOption = (typeof BankList)[number];
+export type BankListOption = {
+  label: string;
+  value: string;
+  color: string;
+  backgroundColor: string;
+};
